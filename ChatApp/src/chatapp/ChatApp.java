439be7a30e713e0ed17005e348c71a003b84f977
Ibrahim <ibi456@hotmail.com>
2016-10-02 22:42:39 +1100

    discover connected hosts
    Added new IP4Address with extended functionality

--- ChatApp/src/chatapp/ChatApp.java
+++ ChatApp/src/chatapp/ChatApp.java
@@ -5,6 +5,8 @@
  */
 package chatapp;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,8 +25,29 @@ public class ChatApp
         ibisTest();
     }
     
-    static void ibisTest()
+    static void ibisTest() 
     {
+        ServerSocket socket=null;
+        try
+        {
+            socket = new ServerSocket(4009);
+        } catch (IOException ex)
+        {
+            Logger.getLogger(ChatApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally
+        {
+           if(socket!=null)
+           {
+               try
+               {
+                   socket.close();
+               } catch (IOException ex)
+               {
+                   Logger.getLogger(ChatApp.class.getName()).log(Level.SEVERE, null, ex);
+               }
+           }
+        }
         Thread t = new Discovery();
         t.start();
         
--- ChatApp/src/chatapp/Discovery.java
+++ ChatApp/src/chatapp/Discovery.java
@@ -5,13 +5,19 @@
  */
 package chatapp;
 
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import sun.security.pkcs11.wrapper.Functions;
+//using subnet utils provided by apache
+import  org.apache.commons.net.util.SubnetUtils;
 
 
 /**
@@ -21,10 +27,19 @@ import sun.security.pkcs11.wrapper.Functions;
  */
 public class Discovery extends Thread
 {
+    private ArrayList<IP4Address> connectedHosts;
+    private HashMap<String,IP4Address> groupChatHosts;
     private Host localHost;
-    
+
+    public Host getLocalHost()
+    {
+        return localHost;
+    }
+    private final int IP_RANGE=10;
+    private final int FILE_TRANSFER_PORT=4009;
     public Discovery()
     {
+        connectedHosts= new ArrayList<>();
         try
         {
             
@@ -88,32 +103,107 @@ public class Discovery extends Thread
         //get bits of network address
         NetworkInterface networkInterface = NetworkInterface.getByInetAddress(localHost.getHostIP());
         short subnetBits=networkInterface.getInterfaceAddresses().get(0).getNetworkPrefixLength();
-            
         
-               
+        //create IP4 class with address annd subnet
+        IP4Address myAddress= new IP4Address(localHost.getHostIP().getHostName(), String.valueOf(subnetBits));
         
-
+        //check hosts for subnet in a loop every T seconds
+        checkHosts(myAddress);
         
     }
-    private void checkHosts(String subnet)
+    public synchronized void addToChatGroup(String hostname,IP4Address address)
     {
+        //TO DO updating the host lists for ARE U ONLINE confirmation
+    }
+    private void checkHosts(IP4Address address) throws Exception
+    {
+        //iterate through IP RANGE to check if connected
+        IP4Address nextAdd=address;
+        IP4Address previousAdd=address;
         int timeout=1000;
-        for (int i=1;i<255;i++)
+        for(int i=0;i<IP_RANGE;i++)
         {
-            String host=subnet + "." + i;
-            try
+            if(nextAdd.hasNext())
             {
-                if (InetAddress.getByName(host).isReachable(timeout))
-            {
-                System.out.println(host + " is reachable");
-            }
-            } catch (Exception e)
-            {
-                System.err.println("Exception Finding host");
-                System.err.println(e.getMessage());
+              //get next host to ping
+              nextAdd=nextAdd.next(); 
+                System.out.println("pingig"+nextAdd.toString());
+              if(pingHostApplication(nextAdd))
+              {
+                  connectedHosts.add(nextAdd);
+                  System.out.println(nextAdd.toString()+"Reachable");
+              }
+                
             }
             
+            if(previousAdd.hasPrevious())
+            {
+                //get previous host
+                previousAdd=previousAdd.previous();
+                System.out.println("pingig"+previousAdd.toString());
+                if(pingHostApplication(previousAdd))
+                {
+                    System.out.println(previousAdd.toString()+"Reachable");
+                }
+            }
+                
         }
+        
+        System.out.println(address);
+        
+        
+        
+        System.out.println(address.previous());
+      
+        
+//        for (int i=1;i<255;i++)
+//        {
+//            String host=subnet + "." + i;
+//            try
+//            {
+//                if (InetAddress.getByName(host).isReachable(timeout))
+//            {
+//                System.out.println(host + " is reachable");
+//            }
+//            } catch (Exception e)
+//            {
+//                System.err.println("Exception Finding host");
+//                System.err.println(e.getMessage());
+//            }
+//            
+//        }
     }
     
+    private boolean pingHostApplication(IP4Address address) 
+    {
+        //tries to create a connection to FILE_TRANFER_PORT
+        //host using this application has to keep this port open
+        Socket testSock=null;
+        try
+        {
+            InetAddress inet=address.getInetAddress();
+             testSock= new Socket();
+             testSock.connect(new InetSocketAddress(inet,FILE_TRANSFER_PORT), 1000);
+        } catch (UnknownHostException ex)
+        {
+            Logger.getLogger(Discovery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        catch(IOException e)
+        {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        finally
+        {
+            try
+            {
+                if(testSock!=null)
+                testSock.close();
+            } catch (IOException ex)
+            {
+                Logger.getLogger(Discovery.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return true;
+    }
 }
