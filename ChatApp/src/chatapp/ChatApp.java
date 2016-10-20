bc72f5937fc001ffc01f85b74c386493f0390461
Ibrahim <ibi456@hotmail.com>
2016-10-20 14:09:33 +1100

    final updates

--- ChatApp/src/chatapp/ChatApp.java
+++ ChatApp/src/chatapp/ChatApp.java
@@ -10,8 +10,13 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.UnknownHostException;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import static java.util.logging.Logger.getLogger;
+import static java.util.logging.Logger.getLogger;
+import java.util.logging.SimpleFormatter;
 
 /**
  *
@@ -20,59 +25,48 @@ import java.util.logging.Logger;
 public class ChatApp
 {
     private static final String grpCode="IFFY";
-
-    /**
+    private static FileHandler fileTxt;
+    public static final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+    /** initiate chat program
      * @param args the command line arguments
      */
     public static void main(String[] args)
     {
+        //read username from command line
+       
+        //logger setup
+        SimpleFormatter formatter= new SimpleFormatter();
         
+        Logger globalLogger = Logger.getLogger("");
+        Handler[] handlers = globalLogger.getHandlers();
+        for(Handler handler : handlers) {
+        globalLogger.removeHandler(handler);
+}
+       
        
        String username="Ibrahim";
         MessageProcessor msgproc= new MessageProcessor(username,grpCode);
         msgproc.start();
 
         try {
-            msgproc.join();           
+             fileTxt= new FileHandler("logs.txt");
+             
+             logger.addHandler(fileTxt);
+             logger.warning("Log File Created");
+            msgproc.join();      
+           
             //ibisTest();
         } catch (InterruptedException ex) {
+            Logger.getLogger(ChatApp.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex)
+        {
+            Logger.getLogger(ChatApp.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex)
+        {
             Logger.getLogger(ChatApp.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    static void srisTest()
-    {
-//        Thread s = new CommandLine();
-//        s.start();
-//        
-//        try
-//        {
-//            s.join();
-//        } catch (InterruptedException ex)
-//        {
-//            Logger.getLogger(ChatApp.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//
-//        Thread m = new MessageProcessor();
-//        m.start();
-//        
-//        try
-//        {
-//            m.join();
-//        } catch (InterruptedException ex)
-//        {
-//            Logger.getLogger(ChatApp.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-        
-    }
     
-    static void ibisTest()
-
-
-    {
-        ServerSocket socket=null;
-        
-        
-    }
 
 }
